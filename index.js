13848c5d5bdabcb7bc2ecd63cafe6367c9fee06e
tylernpc <103677076+tylernpc@users.noreply.github.com>
2023-10-15 23:19:00 -0500

    Jan Ken Rock is complete, now I'm just styling for fun and potentially adding a best of three feature.

--- index.js
+++ index.js
@@ -1,9 +1,29 @@
+for (let i = 0; i <= 3; i++) {
+    
+}
+
 function getComputerChoice() {
-    computerChoices = ["Rock", "Paper", "Scissors"];
+    computerChoices = ['Rock', 'Paper', 'Scissors'];
     const randomChoice = Math.floor(Math.random() * computerChoices.length);
     return computerChoices[randomChoice];
 }
-
 const computerChoice = getComputerChoice();
-console.log(computerChoice);
 
+function resultText(result) {
+    document.getElementById('result').innerHTML = result;
+}
+
+function userInput(userChoice) {
+    if (userChoice == computerChoice) {
+        resultText("It's a draw!");
+    } else if ((userChoice == 'Rock' && computerChoice == 'Scissors') ||
+    (userChoice == 'Paper' && computerChoice == 'Rock') ||
+    (userChoice == 'Scissors' && computerChoice == 'Paper')) {
+        resultText("You win!");
+    } else {
+        resultText('You lose!');
+    }
+}
+
+// if you care to see the computers answer, you can check the log!
+console.log(computerChoice);
