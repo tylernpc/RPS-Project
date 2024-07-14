b217e7c0cfaf2ebd7898be8568845dd903292a04
tylernpc <103677076+tylernpc@users.noreply.github.com>
2024-07-14 11:13:11 -0500

    Added a new Scoreboard, This project is officially Complete

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 let computerChoice;
+let userScore = 0;
+let cpuScore = 0;
 
 function getComputerChoice() {
     computerChoices = ['Rock', 'Paper', 'Scissors'];
@@ -12,6 +14,8 @@ function reset() {
 
 function resultText(result) {
     document.getElementById('result').innerHTML = result;
+    document.getElementById('userScore').innerHTML = `User Score: ${userScore}`;
+    document.getElementById('cpuScore').innerHTML = `Computer Score: ${cpuScore}`;
 }
 
 function userInput(userChoice) {
@@ -21,16 +25,13 @@ function userInput(userChoice) {
     } else if ((userChoice == 'Rock' && computerChoice == 'Scissors') ||
     (userChoice == 'Paper' && computerChoice == 'Rock') ||
     (userChoice == 'Scissors' && computerChoice == 'Paper')) {
+        userScore++;
         resultText("You win!");
     } else {
+        cpuScore++;
         resultText('You lose!');
     }
 }
 
-function scoreCounter() {
-
-    while()
-
-    const userScore = 
-    const botScore =
-}
+document.getElementsById('userScore').innerHTML = `User Score ${userScore}`;
+document.getElementsById('cpuScore').innerHTML = `Computer Score ${cpuScore}`;
